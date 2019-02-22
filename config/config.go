2ce3d3131910cb6fcb5f9083272fec160fdf8ff5
Marc Carmier <mcarmier@gmail.com>
2019-02-22 08:12:25 +0100

    Refactor config package

--- config/config.go
+++ config/config.go
@@ -1,26 +1,38 @@
 package config
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+
 	"github.com/spf13/viper"
 )
 
 // Default value
 var defaults = map[string]interface{}{
-	"out_file":    "/tmp/jocasta_stdout.log",
+	"out_file":    "/tmp/jocasta_{{.App}}_stdout.log",
 	"out_maxsize": "0",
 	"out_backups": "0",
-	"err_file":    "/tmp/jocasta_stderr.log",
+	"err_file":    "/tmp/jocasta_{{.App}}_stderr.log",
 	"err_maxsize": "0",
 	"err_backups": "0",
 }
 
 // Config implements the config store of jocasta
 type Config struct {
-	v *viper.Viper
+	v   *viper.Viper
+	App string
+}
+
+// Params type for characteristics of a stream
+type Params struct {
+	File    string
+	Maxsize uint
+	Backups int
 }
 
 // New initialize the config store
-func New(path string, filename string) (*Config, error) {
+func New(path string, filename string, app string) (*Config, error) {
 	v := viper.New()
 	for key, value := range defaults {
 		v.SetDefault(key, value)
@@ -32,6 +44,79 @@ func New(path string, filename string) (*Config, error) {
 	v.AutomaticEnv()
 	err := v.ReadInConfig()
 
-	config := &Config{v: v}
+	config := &Config{v: v, App: app}
 	return config, err
+}
+
+func keyName(key, subkey string) (string, error) {
+	switch key {
+	case "out", "err":
+		return fmt.Sprintf("%s_%s", key, subkey), nil
+	default:
+		return "", fmt.Errorf("Don't know anything about %s", key)
+	}
+
+}
+
+// File return the filename for logs for given stream
+func (c *Config) File(stream string) (string, error) {
+	key, err := keyName(stream, "file")
+	if err != nil {
+		return "", err
+	}
+
+	t, err := template.New("filename").Parse(c.v.GetString(key))
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, c); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
+
+// MaxSize return the max size of log file before rotation for given stream
+func (c *Config) MaxSize(stream string) (uint, error) {
+	key, err := keyName(stream, "maxsize")
+	if err != nil {
+		return 0, err
+	}
+
+	return c.v.GetSizeInBytes(key), nil
+}
+
+// Backups return the number of historical files for logs for given stream
+func (c *Config) Backups(stream string) (int, error) {
+	key, err := keyName(stream, "backups")
+	if err != nil {
+		return 0, err
+	}
+	return c.v.GetInt(key), nil
+}
+
+// GetParams return the whole logs info for given stream in Params type
+func (c *Config) GetParams(stream string) (*Params, error) {
+
+	maxsize, err := c.MaxSize(stream)
+	if err != nil {
+		return nil, err
+	}
+	backups, err := c.Backups(stream)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := c.File(stream)
+	if err != nil {
+		return nil, err
+	}
+	p := &Params{
+		Maxsize: maxsize,
+		Backups: backups,
+		File:    file,
+	}
+	return p, nil
 }
---
+++ config/config_test.go
@@ -0,0 +1,221 @@
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+func TestOutDefaultValue(t *testing.T) {
+	defaultFile := "/tmp/jocasta_dummy_stdout.log"
+	var defaultSize uint
+	defaultBackups := 0
+
+	c, err := New("testdata", "empty", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	maxsize, err := c.MaxSize("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_maxsize. err=%v", err)
+	}
+	if maxsize != defaultSize {
+		t.Errorf("Default value for out.maxsize is not correct, should be %d and I receive %d", defaultSize, maxsize)
+	}
+
+	backups, err := c.Backups("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_backups. err=%v", err)
+	}
+	if backups != defaultBackups {
+		t.Errorf("Default value for out.backups is not correct, should be %d and I receive %d", defaultBackups, backups)
+	}
+
+	file, err := c.File("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_file. err=%v", err)
+	}
+	if file != defaultFile {
+		t.Errorf("Default value for out.file is not correct, should be %s and I receive %s", defaultFile, file)
+	}
+}
+
+func TestOutRetrievedFromFile(t *testing.T) {
+	defaultFile := "/tmp/correct.log"
+	var defaultSize uint = 1024 * 1024
+	defaultBackups := 2
+
+	c, err := New("testdata", "correct", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	maxsize, err := c.MaxSize("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_maxsize. err=%v", err)
+	}
+	if maxsize != defaultSize {
+		t.Errorf("Retrieved value for out.maxsize is not correct, should be %d and I receive %d", defaultSize, maxsize)
+	}
+
+	backups, err := c.Backups("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_backups. err=%v", err)
+	}
+	if backups != defaultBackups {
+		t.Errorf("Retrieved value for out.backups is not correct, should be %d and I receive %d", defaultBackups, backups)
+	}
+
+	file, err := c.File("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_file. err=%v", err)
+	}
+	if file != defaultFile {
+		t.Errorf("Retrieved value for out.file is not correct, should be %s and I receive %s", defaultFile, file)
+	}
+}
+
+func TestErrDefaultValue(t *testing.T) {
+	defaultFile := "/tmp/jocasta_dummy_stderr.log"
+	var defaultSize uint
+	defaultBackups := 0
+
+	c, err := New("testdata", "empty", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	maxsize, err := c.MaxSize("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_maxsize. err=%v", err)
+	}
+	if maxsize != defaultSize {
+		t.Errorf("Default value for err.maxsize is not correct, should be %d and I receive %d", defaultSize, maxsize)
+	}
+
+	backups, err := c.Backups("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_backups. err=%v", err)
+	}
+	if backups != defaultBackups {
+		t.Errorf("Default value for err.backups is not correct, should be %d and I receive %d", defaultBackups, backups)
+	}
+
+	file, err := c.File("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_file. err=%v", err)
+	}
+	if file != defaultFile {
+		t.Errorf("Default value for err.file is not correct, should be %s and I receive %s", defaultFile, file)
+	}
+}
+
+func TestErrFromFile(t *testing.T) {
+	defaultFile := "/tmp/correct_err.log"
+	var defaultSize uint = 2 * 1024 * 1024
+	defaultBackups := 3
+
+	c, err := New("testdata", "correct", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	maxsize, err := c.MaxSize("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_maxsize. err=%v", err)
+	}
+	if maxsize != defaultSize {
+		t.Errorf("Retrieved value for err.maxsize is not correct, should be %d and I receive %d", defaultSize, maxsize)
+	}
+
+	backups, err := c.Backups("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_backups. err=%v", err)
+	}
+	if backups != defaultBackups {
+		t.Errorf("Retrieved value for err.backups is not correct, should be %d and I receive %d", defaultBackups, backups)
+	}
+
+	file, err := c.File("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_file. err=%v", err)
+	}
+	if file != defaultFile {
+		t.Errorf("Retrieved value for err.file is not correct, should be %s and I receive %s", defaultFile, file)
+	}
+}
+
+func TestParams(t *testing.T) {
+	defaultFile := "/tmp/correct_err.log"
+	var defaultSize uint = 2 * 1024 * 1024
+	defaultBackups := 3
+
+	c, err := New("testdata", "correct", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	p, err := c.GetParams("err")
+	if err != nil {
+		t.Errorf("Error while retrieving err_maxsize. err=%v", err)
+	}
+	if p.Maxsize != defaultSize {
+		t.Errorf("Retrieved value for err.maxsize is not correct, should be %d and I receive %d", defaultSize, p.Maxsize)
+	}
+
+	if p.Backups != defaultBackups {
+		t.Errorf("Retrieved value for err.backups is not correct, should be %d and I receive %d", defaultBackups, p.Backups)
+	}
+
+	if p.File != defaultFile {
+		t.Errorf("Retrieved value for err.file is not correct, should be %s and I receive %s", defaultFile, p.File)
+	}
+}
+func TestEnv(t *testing.T) {
+
+	str := "dummy.log"
+
+	os.Setenv("JOCASTA_OUT_FILE", str)
+
+	c, err := New("testdata", "empty", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	file, err := c.File("out")
+	if err != nil {
+		t.Errorf("Error while retrieving out_file. err=%v", err)
+	}
+	if file != str {
+		t.Errorf("Not able to retrieve out.file from environment variable . Found=%s for '%s'", file, str)
+	}
+
+}
+
+func TestWrongStream(t *testing.T) {
+
+	c, err := New("testdata", "empty", "dummy")
+	if err != nil {
+		t.Errorf("Can not read the configuration file. err=%v", err)
+	}
+
+	_, err = c.MaxSize("dummy")
+	if err == nil {
+		t.Errorf("No error while retrieving dummy_file. err=%v", err)
+	}
+
+	_, err = c.Backups("dummy")
+	if err == nil {
+		t.Errorf("No error while retrieving dummy_file. err=%v", err)
+	}
+
+	_, err = c.File("dummy")
+	if err == nil {
+		t.Errorf("No error while retrieving dummy_file. err=%v", err)
+	}
+
+	_, err = c.GetParams("dummy")
+	if err == nil {
+		t.Errorf("No error while retrieving dummy params. err=%v", err)
+	}
+}
