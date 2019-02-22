66cef801e043e63b7eb26065b2c35462fdafc3eb
Marc Carmier <mcarmier@gmail.com>
2019-02-22 16:21:03 +0100

    First version double pipe without limits and rotation

---
+++ logwriter/logwriter.go
@@ -0,0 +1,58 @@
+package logwriter
+
+import (
+	"bytes"
+	"os"
+
+	"github.com/marema31/jocasta/config"
+)
+
+// LogSaver object used to save log to a file with limits and automatic rotation
+type LogWriter struct {
+	params      *config.Params
+	currentsize int
+	file        *os.File
+}
+
+// New instanciate a LogSaver object
+func New(stream string, c *config.Config) (*LogWriter, error) {
+	p, err := c.GetParams(stream)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Create(p.File)
+	if err != nil {
+		return nil, err
+	}
+
+	l := &LogWriter{
+		params:      p,
+		currentsize: 0,
+		file:        f,
+	}
+
+	return l, nil
+}
+
+func (l *LogWriter) Write(p []byte) (int, error) {
+
+	n := 0
+	for n < len(p) {
+		i := bytes.IndexByte(p[n:], byte('\n'))
+		if i == -1 {
+			l.file.Write(p[n:])
+			n = len(p)
+			l.currentsize += len(p) - n
+		} else {
+			l.file.Write(p[n : n+i+1])
+			n += i + 1
+			l.currentsize += i + 1
+		}
+	}
+	return n, nil
+}
+
+func (l *LogWriter) Close() error {
+	return l.file.Close()
+}
--- main.go
+++ main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
 
+	"github.com/marema31/jocasta/config"
+	"github.com/marema31/jocasta/logwriter"
+)
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "-h" {
+		fmt.Println("usage: jocasta command to run with args")
+		os.Exit(0)
+	}
+
+	config, err := config.New(".", "jocasta", os.Args[1])
+
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logerr, err := logwriter.New("err", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logout, err := logwriter.New("out", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	// cmd.Wait() should be called only after we finish reading
+	// from stdout and stderr
+	// wg ensures that we finish
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go transfer(stdout, logout, &wg)
+	go transfer(stderr, logerr, &wg)
+
+	wg.Wait()
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("Unable to exec %s: %s\n", os.Args[1], err)
+	}
+}
+
+func transfer(in io.ReadCloser, out io.Writer, wg *sync.WaitGroup) {
+	if _, err := io.Copy(out, in); err != nil {
+		log.Fatal(err)
+	}
+	wg.Done()
 }
